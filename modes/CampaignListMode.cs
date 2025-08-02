50edd52fe19c9bd5598dcf43ea18c5fe19345edc
Andrew Korchinsky <ak47reborntumpa@gmail.com>
2025-08-02 17:34:45 +0300

    feat: add mode structure diagram and scaffolding
    - Added Excalidraw diagram visualizing mode transitions and relationships
    - Created scaffolding classes for CampaignList, Campaign, and MapList modes
    - Documented debug mode entry condition in diagram

---
+++ modes/CampaignListMode.cs
@@ -0,0 +1,14 @@
+namespace Vtt.Modes;
+
+public class CampaignListMode : Mode
+{
+    public override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ modes/CampaignMode.cs
@@ -0,0 +1,14 @@
+namespace Vtt.Modes;
+
+public class CampaignMode : Mode
+{
+    public override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+}
