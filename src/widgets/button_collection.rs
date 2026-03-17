1e6972df4cc23d2f5d6900512e7964e2d62e42a6
Your Name <you@example.com>
2026-03-17 14:43:12 +0300

    Fucking draft. Fix later

---
+++ src/widgets/button_collection.rs
@@ -0,0 +1,86 @@
+use raylib::drawing::RaylibDrawHandle;
+use raylib::math::Vector2;
+use raylib::RaylibHandle;
+use crate::config::BASE_UI_SCALE;
+use crate::widgets::{Button, Widget};
+use crate::widgets::button::MinimalButton;
+
+pub struct ButtonCollection<Event> {
+    buttons: Vec<Button<Event>>,
+}
+
+impl<Event> ButtonCollection<Event> {
+    pub fn new(pos: Vector2, grid: Vec<Vec<Option<MinimalButton<Event>>>>, config: ButtonCollectionConfig) -> ButtonCollection<Event> {
+        let mut buttons = vec![];
+
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                let button = match grid[i][j] { // FIXME: will not compile
+                    None => {continue;}
+                    Some(mb) => {Button::new(
+                        pos + Vector2::new(j as f32 * (config.horizontal_spacing as f32 + config.size.x), i as f32 * (config.vertical_spacing as f32 + config.size.y)),
+                        config.size,
+                        mb
+                    )}
+                };
+                buttons.push(button);
+            }
+        }
+
+        ButtonCollection { buttons }
+    }
+
+    pub fn handle_mouse(&mut self, rl: &RaylibHandle, mouse_pos: Option<Vector2>) -> Vec<Event> {
+        let mut events = vec![];
+        for button in &mut self.buttons {
+            events.push(button.handle_mouse(rl, mouse_pos));
+        }
+
+        events.into_iter().flatten().collect()
+    }
+}
+
+pub struct ButtonCollectionConfig {
+    size: Vector2,
+    horizontal_spacing: u32,
+    vertical_spacing: u32,
+}
+
+impl ButtonCollectionConfig {
+    pub fn new() -> ButtonCollectionConfig {
+        ButtonCollectionConfig {
+            size: Vector2::new(BASE_UI_SCALE as f32, BASE_UI_SCALE as f32),
+            horizontal_spacing: BASE_UI_SCALE,
+            vertical_spacing: BASE_UI_SCALE,
+        }
+    }
+
+    pub fn with_size(mut self, size: Vector2) -> ButtonCollectionConfig {
+        self.size = size;
+        self
+    }
+
+    pub fn with_horizontal_spacing(mut self, horizontal_spacing: u32) -> ButtonCollectionConfig {
+        self.horizontal_spacing = horizontal_spacing;
+        self
+    }
+
+    pub fn with_vertical_spacing(mut self, vertical_spacing: u32) -> ButtonCollectionConfig {
+        self.vertical_spacing = vertical_spacing;
+        self
+    }
+}
+
+impl<Event> Widget for ButtonCollection<Event> {
+    fn draw(&self, d: &mut RaylibDrawHandle) {
+        for button in &self.buttons {
+            button.draw(d);
+        }
+    }
+
+    fn update(&mut self, rl: &RaylibHandle, dt: f32) {
+        for button in &mut self.buttons {
+            button.update(rl, dt);
+        }
+    }
+}
