120a9b750c5b284ce3529a647a31ed32a2bb8fe5
AK-47 <161627201+ak47andrew@users.noreply.github.com>
2026-03-21 13:59:04 +0300

    Added ChoiceCard stub. Gonna figure out Raylib stuff now

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -2,8 +2,10 @@ pub mod button;
 pub mod widget;
 mod button_style;
 pub mod button_collection;
+pub mod choice_card;
 
 pub use widget::Widget;
 pub use button::{Button, ButtonMode};
 pub use button_style::ButtonStyle;
 pub use button_collection::ButtonCollection;
+pub use choice_card::ChoiceCard;
