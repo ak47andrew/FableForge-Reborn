4625fbaf57ae8b122ce2a455ad31269a37c6ad7b
AK-47 <161627201+ak47andrew@users.noreply.github.com>
2026-03-22 02:08:25 +0300

    Added new context system bc raylib-rs can eat my entire ass
    I thought only roll20 can eat ass, but no. Raylib is also in the same boat

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,48 @@ mod widgets;
 
 use raylib::prelude::*;
 use crate::managers::ModeManager;
-use crate::modes::DebugMode;
+use crate::modes::{DebugMode, Mode};
+use crate::utils::Context;
 
 fn main() {
-    let (mut rl, thread) = raylib::init()
+    let (mut rl, mut thread) = init()
         .vsync()
         .size(640, 480)
         .title("Hello, World")
         .build();
-
-    let mut mode_manager = ModeManager::new(Box::new(DebugMode::new()));
+    let mut mode_manager = {
+        let mut context = Context::new(&mut rl, &mut thread);
+        ModeManager::new(Box::new(DebugMode::new(&mut context)))
+    };
 
     let mut return_to_debug = 0.0f32;
 
     while !rl.window_should_close() {
         let dt = rl.get_frame_time();
+        {
 
-        if config::DEBUG {
-            if rl.is_key_down(KeyboardKey::KEY_ENTER) && rl.is_key_down(KeyboardKey::KEY_BACKSPACE) &&
-                mode_manager.get_current_mode().as_any().downcast_ref::<DebugMode>().is_none() {
-                return_to_debug += dt;
-                println!("Returning to DebugMode in {} seconds...", config::ENTER_DEBUG_TIME - return_to_debug);
-                if return_to_debug > config::ENTER_DEBUG_TIME {
-                    mode_manager.set_mode(Box::new(DebugMode::new()));
+            if config::DEBUG {
+                if rl.is_key_down(KeyboardKey::KEY_ENTER) && rl.is_key_down(KeyboardKey::KEY_BACKSPACE) &&
+                    mode_manager.get_current_mode().as_any().downcast_ref::<DebugMode>().is_none() {
+                    let mut context = Context::new(&mut rl, &mut thread);
+                    return_to_debug += dt;
+                    println!("Returning to DebugMode in {} seconds...", config::ENTER_DEBUG_TIME - return_to_debug);
+                    if return_to_debug > config::ENTER_DEBUG_TIME {
+                        mode_manager.set_mode(Box::new(DebugMode::new(&mut context)));
+                        return_to_debug = 0.0;
+                    }
+                } else {
                     return_to_debug = 0.0;
                 }
-            } else {
-                return_to_debug = 0.0;
             }
+
+
+            let mut context = Context::new(&mut rl, &mut thread);
+            mode_manager.get_current_mode().update(&mut context, dt);
         }
-
-        let current_mode = mode_manager.get_current_mode();
-
-        current_mode.update(&rl, dt);
 
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::DARKGRAY);
-        current_mode.draw(&mut d);
+        mode_manager.get_current_mode().draw(&mut d);
     }
 }
